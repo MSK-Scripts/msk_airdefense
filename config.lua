263bd4d2bd90d2485f08c3f8b3ac30292cd1502e
cmd <123102218+msk-cmd@users.noreply.github.com>
2023-01-19 18:21:42 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,17 @@
 Config = {}
 
 Config.Zones = {
-    {pos = vector3(1689.3793, 2595.6357, 72.8098), radius = 300.0, allowedJobs = {"police", "ambulance"}},
+    {
+        pos = vector3(1689.3793, 2595.6357, 72.8098), 
+        radius = 300.0, 
+        allowedJobs = {"police", "ambulance"}, 
+        time = 10, -- in seconds, time until vehicle failure
+        Notify = {
+            header = 'Flugverbotszone',
+            subject = '',
+            msg = 'Du befindest dich in einer Flugverbotszone. Du hast %s Sekunde(n) um umzukehren!',
+            mugshot = 'CHAR_MP_MERRYWEATHER',
+            iconType = 1
+        }
+    },
 }
