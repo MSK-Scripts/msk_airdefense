a04f21973f9ee4fe03bfad9f13ebd12419b6a892
msk-cmd <123102218+msk-cmd@users.noreply.github.com>
2023-01-19 17:51:55 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config = {}
 Config = {
 
     Zone = {
-        {pos = vector3(1689.3793, 2595.6357, 72.8098), radius = 300.0, AllowJob = {"ambulance", "police"}},
+        {pos = vector3(1689.3793, 2595.6357, 72.8098), radius = 300.0, AllowJob = {"ambulance"}},
 
     },
 
