a4b943bed8b113225f45037cec00132b7f56769d
cmd <123102218+msk-cmd@users.noreply.github.com>
2023-01-19 19:00:11 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,32 +1,45 @@
-ESX = exports['es_extended']:getSharedObject()
+ESX = exports["es_extended"]:getSharedObject()
+
+local isSet = false
 
 CreateThread(function()
     while true do
-        local wait = 1000
+        local sleep = 1000
         local playerPed = PlayerPedId()
         local playerCoords = GetEntityCoords(playerPed)
         local vehicle = GetVehiclePedIsIn(playerPed, false) 
 
         for k, v in pairs(Config.Zones) do
             local pos = v.pos
-            local dist = #(playerCoords - v.radius)
+            local dist = #(pos - playerCoords)
 
-            if dist <= v.radius then
-                ESX.ShowAdvancedNotification(v.header, v.subject, (v.msg):format(v.time), v.mugshot, v.iconType)
-                Wait(v.time * 1000)
-
-                if ESX.PlayerData and ESX.PlayerData.job and table.contains(v.allowedJobs, ESX.PlayerData.job.name) then
-                    if GetVehicleClass(vehicle) == 16 or GetVehicleClass(vehicle) == 15 then
-                        -- SetVehicleEngineHealth(vehicle, 10)
-                        SetVehicleEngineOn(vehicle, false, true, true)
-                    end
-                end
+            if dist <= v.radius and not isSet then
+                sleep = v.time
+                isSet = true
+                setEngineFailure(vehicle, v)
+            elseif dist > v.radius and isSet then
+                isSet = false
             end
         end
         
-        Wait(wait)
+        Wait(sleep)
     end
 end)
+
+setEngineFailure = function(vehicle, v)
+    if ESX.PlayerData and ESX.PlayerData.job and not table.contains(v.allowedJobs, ESX.PlayerData.job.name) then
+        ESX.ShowAdvancedNotification(v.Notify.header, v.Notify.subject, v.Notify.msg:format(v.time), v.Notify.mugshot, v.Notify.iconType)
+        Wait(v.time * 1000)
+
+        if GetVehicleClass(vehicle) == 16 or GetVehicleClass(vehicle) == 15 then
+            SetVehicleEngineHealth(vehicle, 500)
+            Wait(v.timeLow * 1000)
+            SetVehicleEngineHealth(vehicle, 300)
+            Wait(v.timeLow * 1000)
+            SetVehicleEngineOn(vehicle, false, true, true)
+        end
+    end
+end
 
 table.contains = function(t, value)
     for k, v in pairs(t) do
